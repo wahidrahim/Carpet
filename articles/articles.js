85bb8391c23b7668851cb7eb3cbb516327edc735
Wahid Rahim <wahidrahim@gmail.com>
2016-02-21 23:49:58 -0500

    a new static blog is born

---
+++ articles/articles.js
@@ -0,0 +1,37 @@
+var ARTICLES_DIR = '_articles';
+
+var path = require('path');
+var fs = require('fs');
+var fm = require('front-matter');
+
+var articles = [];
+
+// Load articles on startup
+
+fs.readdirSync(ARTICLES_DIR).forEach(function(file) {
+  var article = fs.readFileSync(path.join(ARTICLES_DIR, file), 'utf8');
+
+  article = format(article);
+  articles.push(article);
+});
+
+articles.sort(function(a, b) {
+  return new Date(b.date) - new Date(a.date);
+});
+
+function format(article) {
+  var fma = fm(article); // article with front-matter data
+  var fmt = {}; // blank object to turn into formatted article
+
+  fmt.title = fma.attributes.title;
+  fmt.date = fma.attributes.date;
+  fmt.url = fma.attributes.url;
+  fmt.body = fma.body;
+
+  if (fmt.url === undefined)
+    fmt.url = fmt.title.split(' ').join('-').toLowerCase();
+
+  return fmt;
+}
+
+module.exports = articles;
